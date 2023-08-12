b2c7293f2d7a971538e98071da9043a29f5acfac
idoporges <82283830+idoporges@users.noreply.github.com>
2023-08-12 12:06:42 +0300

    v1 print

--- main.py
+++ main.py
@@ -365,7 +365,6 @@ if __name__ == '__main__':
     text_generator = valid_text_generator(author)
     for _ in text_generator:
         pass
-    print("test")
     print("test_________________________")
     text_generator = test_text_generator(author)
     for _ in text_generator:
